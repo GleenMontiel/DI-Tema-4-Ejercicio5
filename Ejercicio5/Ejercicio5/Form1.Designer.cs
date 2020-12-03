976857dc1697aa7d12045ae567a2562b31c781f0
GleenMontiel <65775735+GleenMontiel@users.noreply.github.com>
2020-12-03 19:32:30 +0100

    First Commit

---
+++ Ejercicio5/Ejercicio5/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Ejercicio5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(184, 324);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(501, 67);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(203, 324);
+            this.listBox2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Quitar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(212, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Traspasar de \"A\" a \"B\"";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(240, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(212, 34);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Traspasar de \"B\" a \"A\"";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(674, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Cantidad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(499, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Indices seleccionados:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1742, 1026);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Ejercicio5/Ejercicio5/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio5
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() != "") 
+            {
+                listBox1.Items.Add(textBox1.Text.Trim());
+                label1.Text = listBox1.Items.Count.ToString();
+                textBox1.Text = "";
+                ActualizarIndice();
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+            ActualizarIndice();
+        }
+
+        public void ActualizarIndice()
+        {
+            label2.Text = "";
+            if (listBox1.SelectedItems.Count != 0)
+            {
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    label2.Text += listBox1.Items.IndexOf(listBox1.SelectedItems[i]) + ",";
+                }
+                label2.Text = label2.Text.Substring(0, label2.Text.Length - 1);
+            }
+        }
+
+        public void Eliminar() 
+        {
+            if (listBox1.SelectedItems.Count != 0)
+            {
+                ListBox.SelectedObjectCollection seleccionados = listBox1.SelectedItems;
+
+                for (int i = seleccionados.Count - 1; i >= 0; i--)
+                {
+                    listBox1.Items.RemoveAt(listBox1.Items.IndexOf(seleccionados[i]));
+                }
+                label1.Text = listBox1.Items.Count.ToString();
+                ActualizarIndice();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedObjectCollection seleccionados = listBox1.SelectedItems;
+            for (int i = 0; i < seleccionados.Count; i++)
+            {
+                listBox2.Items.Insert(0, seleccionados[i]);
+            }
+            label2.Text = listBox2.Items.Count.ToString();
+            Eliminar();
+            ActualizarIndice();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItems.Count != 0)
+            {
+                listBox1.Items.Insert(0, listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+                ActualizarIndice();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ActualizarIndice();
+        }
+    }
+}
